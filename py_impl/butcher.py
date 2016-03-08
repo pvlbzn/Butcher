d3360384d02788ec3d6967c86e8504f49440fea3
pvlbzn <pvlbzn@gmail.com>
2016-03-08 03:24:23 +0300

    Clean up in python

--- py_impl/butcher.py
+++ py_impl/butcher.py
@@ -3,8 +3,6 @@ from PIL import Image
 
 import time
 
-def open_img(address):
-    return Image.open(address)
 
 def downscale_size(img, scale):
     nsize = img.size
@@ -18,9 +16,7 @@ def downscale(address, scale, f):
         name = "%s_%.2f.png" % (f, float(scale))
         fname = os.path.abspath("output") + '/' + name
         img.thumbnail(size, Image.ANTIALIAS)
-        print(img.size)
         img.save(fname, "PNG")
-        print("%s saved to %s\n" % (name, os.path.abspath("output")))
 
 def input():
     return os.listdir("input")
