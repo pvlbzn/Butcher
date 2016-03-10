4c3fc493170af32dce15b31896d419a3e1e37851
pvlbzn <pvlbzn@gmail.com>
2016-03-11 00:06:18 +0300

    Remake python impl.

--- py_impl/butcher.py
+++ py_impl/butcher.py
@@ -1,32 +1,72 @@
-import os, sys
-from PIL import Image
-
+import os
+import sys
 import time
 
+from PIL import Image
+from pathlib import Path
 
-def downscale_size(img, scale):
-    nsize = img.size
-    xsize = nsize[0] * scale
-    ysize = nsize[1] * scale
-    return (int(xsize), int(ysize))
+DPI = ['ldpi', 'mdpi', 'hdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi']
+SCALES = {"ldpi": .75, "mdpi": 1.0, "hdpi": 1.5,
+        "xhdpi": 2.0, "xxhdpi": 3.0, "xxxhdpi": 4.0}
 
-def downscale(address, scale, f):
-    with Image.open(address) as img:
-        size = downscale_size(img, scale)
-        name = "%s_%.2f.png" % (f, float(scale))
-        fname = os.path.abspath("output") + '/' + name
-        img.thumbnail(size, Image.ANTIALIAS)
-        img.save(fname, "PNG")
+def files():
+    afiles = os.listdir("input")
+    sfiles = []
+    for f in afiles:
+        if f[len(f)-4:] == ".png":
+            sfiles.append(f)
+    return sfiles
 
-def input():
-    return os.listdir("input")
+def read_args():
+    # Take a first argument, which has to be
+    return sys.argv[1]
 
-def do():
-    stime = time.time()
-    flist = input()
-    for f in flist:
-        downscale("input/" + f, .5, f)
-    print("%s seconds" % (time.time() - stime))
+def get_sizes(img, dpi, dpi_range):
+    size = img.size
+    # Downscale size to mdpi
+    print(size[0], size[1])
+    mdpi_w = size[0] / SCALES[dpi]
+    mdpi_h = size[1] / SCALES[dpi]
+    sizes = {}
+    for scale in SCALES:
+        if scale in dpi_range:
+            w = mdpi_w * SCALES[scale]
+            h = mdpi_h * SCALES[scale]
+            sizes[scale] = (w, h)
+    return sizes
 
-if __name__ == "__main__":
-    do()
+def downscale(img, sizes, name):
+    for key in sizes:
+        folder = '/' + "drawable-" + key + '/'
+        fname = os.path.abspath("output") + folder + name
+        print("Saving file {0} in {1}".format(name, fname))
+        print(key, sizes[key])
+        # Copy img. Otherwise one image will be croped len(sizes) times.
+        i = img.copy()
+        i.thumbnail(sizes[key], Image.ANTIALIAS)
+        i.save(fname, "PNG")
+        i.close()
+
+def check_dirs(sizes):
+    for key in sizes:
+        # TODO: Use different path generation.
+        dir = Path("output/drawable-" + key)
+        if not dir.exists():
+            dir.mkdir()
+
+def main():
+    flist = files()
+    user_dpi = read_args()
+    # Filter out DPI that higher than user's DPI.
+    dpi_range = DPI[:DPI.index(user_dpi) + 1]
+    for fname in flist:
+        addr = "input/" + fname
+        with Image.open(addr) as img:
+            sizes = get_sizes(img, user_dpi, dpi_range)
+            check_dirs(sizes)
+            print("Should be once!")
+            # Check directories for DPIs which will be used.
+            downscale(img, sizes, fname)
+
+if __name__ == '__main__':
+    main()
